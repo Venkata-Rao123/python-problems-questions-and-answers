eca415bdd48f56fd38e20a9366c606781d926453
Venkata-Rao123 <venkataraoneelapala2@gmail.com>
2026-07-30 20:46:24 +0530

    isArmstrong

--- problem-2.py
+++ problem-2.py
@@ -1,2 +1,13 @@
-for i in range(1,10):
-    print(i)
+def isArmstrong(n):
+    str_n = str(n)
+    count_n = len(str_n)
+    sum_n = 0
+    for each_number in str_n:
+        sum_n = sum_n + (int(each_number)**count_n)
+    if n == sum_n:
+        return True
+    else:
+        return False    
+number = int(input("Enter the number: "))
+output = isArmstrong(number)  
+print(output)  
